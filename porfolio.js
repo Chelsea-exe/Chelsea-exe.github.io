6e3e01833dab2e00b9001c5e501af7004fe48b67
Chelsea-exe <cjackson145@student.gsu.edu>
2020-05-22 21:20:41 -0400

    modified portfolio with js for applications live websites

---
+++ porfolio.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $("#trivia-app").click(function() {
+        window.open("https://90strivia.netlify.app/", "_blank")
+        console.log("90's application window tab");
+    });
+    $("#trivia-github").click(function() {
+        window.open("https://github.com/Chelsea-exe/unit-4-game", "_blank")
+        console.log("90's github tab");
+    });
+    $("#gif-github").click(function() {
+        window.open("https://github.com/Chelsea-exe/GifTastic", "_blank")
+        console.log("giphy github tab");
+    });
+    $("#gif-app").click(function() {
+        window.open("https://womengiphy.netlify.app/", "_blank");
+        console.log("giphy app tab");
+    });
+    $("#crystal-github").click(function() {
+        window.open("https://github.com/Chelsea-exe/GifTastic", "_blank");
+        console.log("crystal github tab");
+    });
+    $("#crystal-app").click(function() {
+        window.open("https://github.com/Chelsea-exe/Crystal-Collector-Game", "_blank");
+        console.log("crystal app tab");
+    });
+    $("#negotiator-github").click(function() {
+        window.open("https://github.com/Chelsea-exe/The-Negotiator", "_blank");
+        console.log("negotiator github tab");
+    });
+    $("#negotiator-app").click(function() {
+        window.open("http://thenegotiator.herokuapp.com/", "_blank");
+        console.log("negotiator app tab");
+    });
+    $("#software-github").click(function() {
+        window.open("https://github.com/Chelsea-exe/Software-Engineer-Game", "_blank");
+        console.log("software github tab");
+    });
+    $("#software-app").click(function() {
+        window.open("https://gametestapp1.herokuapp.com/");
+        console.log("software app tab");
+    });
+})
